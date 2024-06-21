cca17586ec11e44f64b72d831b7d5001ca91b279
William <wmeza208@gmail.com>
2024-06-20 23:15:34 -0500

    Primer commit

---
+++ src/sistemaventa/SistemaVenta.java
@@ -0,0 +1,13 @@
+
+package sistemaventa;
+
+import Vista.Login;
+
+public class SistemaVenta {
+
+    public static void main(String[] args) {
+        Login lg = new Login();
+        lg.setVisible(true);
+    }
+    
+}
